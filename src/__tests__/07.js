88bc6076a2fb810b48f77710ef3168e570784c27
Kent C. Dodds <kent@doddsfamily.us>
2019-10-07 21:11:03 -0600

    add test for 07

--- src/__tests__/07.js
+++ src/__tests__/07.js
@@ -1,6 +1,40 @@
-// import Usage from '../exercises-final/01'
-// import Usage from '../exercises/01'
+import React from 'react'
+import chalk from 'chalk'
+import {render} from '@testing-library/react'
+import reportProfile from '../report-profile'
+import Usage from '../exercises-final/07'
+// import Usage from '../exercises/07'
 
-test('TODO', () => {
-  // write this test
+jest.mock('react', () => {
+  const actualReact = jest.requireActual('react')
+  return {
+    ...actualReact,
+    Profiler: jest.fn(({children}) => children),
+  }
+})
+
+test('uses the Profiler correctly', () => {
+  render(<Usage />)
+
+  try {
+    expect(React.Profiler).toHaveBeenLastCalledWith(
+      {
+        children: expect.any(Object),
+        id: 'counter',
+        onRender: reportProfile,
+      },
+      expect.any(Object),
+    )
+  } catch (error) {
+    //
+    //
+    //
+    // these comment lines are just here to keep the next line out of the codeframe
+    // so it doesn't confuse people when they see the error message twice.
+    error.message = `🚨  ${chalk.red(
+      'The React.Profiler component must be used with the correct props',
+    )}\n\n${error.message}`
+
+    throw error
+  }
 })
