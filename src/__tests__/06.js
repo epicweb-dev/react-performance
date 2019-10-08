d6b63efd028b7a6a29e9bd2449672e6a4ff69761
Kent C. Dodds <kent@doddsfamily.us>
2019-10-07 21:02:40 -0600

    add tests for 06

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -1,6 +1,56 @@
-// import Usage from '../exercises-final/01'
-// import Usage from '../exercises/01'
+import React from 'react'
+import chalk from 'chalk'
+import {render} from '@testing-library/react'
+import Usage from '../exercises-final/06'
+// import Usage from '../exercises/06'
 
-test('TODO', () => {
-  // write this test
+jest.mock('react', () => {
+  const actualReact = jest.requireActual('react')
+  return {
+    ...actualReact,
+    useMemo: jest.fn((...args) => actualReact.useMemo(...args)),
+  }
+})
+
+test('memoizes state properly', () => {
+  render(<Usage />)
+  const memoCall = React.useMemo.mock.calls.find(
+    ([fn, deps]) => deps && deps.some && deps.some(d => d && d.grid),
+  )
+  if (!memoCall) {
+    throw new Error(
+      `🚨  ${chalk.red(
+        'AppStateProvider must call React.useMemo with the state as a dependency',
+      )}`,
+    )
+  }
+  const [memoFn, deps] = memoCall
+  const [state, dispatch] = memoFn()
+
+  if (!state || !dispatch) {
+    throw new Error(
+      `🚨  ${chalk.red(
+        'The useMemo callback must return an array that includes the state object and dispatch function',
+      )}`,
+    )
+  }
+
+  if (typeof dispatch !== 'function') {
+    throw new Error(
+      `🚨  ${chalk.red(
+        'The second element in the array returned from the useMemo callback must be the dispatch function',
+      )}`,
+    )
+  }
+
+  if (
+    !(deps.length === 1 && deps.includes(state)) &&
+    !(deps.length === 2 && deps.includes(state) && deps.includes(dispatch))
+  ) {
+    throw new Error(
+      `🚨  ${chalk.red(
+        'The useMemo dependency array must be an array that includes only the state (and optionally the dispatch function)',
+      )}`,
+    )
+  }
 })
