7830e71fc13951966072f908a7abaca852d50766
Kent C. Dodds <kent@doddsfamily.us>
2019-10-31 15:09:27 -0600

    add forgotten link

--- src/exercises/03.js
+++ src/exercises/03.js
@@ -121,6 +121,19 @@ function FilterComponent() {
   )
 }
 
+/*
+🦉 Elaboration & Feedback
+After the instruction, copy the URL below into your browser and fill out the form:
+http://ws.kcd.im/?ws=React%20Performance&e=React.memo&em=
+*/
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                 Don't make changes below here.                 //
+// But do look at it to see how your code is intended to be used. //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+
 function Usage() {
   return <FilterComponent />
 }
