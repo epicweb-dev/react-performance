dacc0c6536d141618e4e019317a86520d595dc4c
Vojta Holik <vojta@egghead.io>
2020-04-21 14:22:52 +0200

    Add Globe Component

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,29 +3,40 @@
 
 import React from 'react'
 // 💣 remove this import
-import Tilt from '../tilt'
+import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Tilt component which using a dynamic import
-// to get the Tilt component from the '../tilt' module.
+// 🐨 use React.lazy to create a Globe component which using a dynamic import
+// to get the Globe component from the '../globe' module.
 
 function App() {
-  const [showTilt, setShowTilt] = React.useState(false)
+  const [showGlobe, setShowGlobe] = React.useState(false)
 
   // 🐨 wrap the code below in a <React.Suspense /> component
   // with a fallback.
   // 💰 try putting it in a few different places and observe how that
   // impacts the user experience.
   return (
-    <div style={{width: 200}}>
-      <label>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label style={{marginBottom: '1rem'}}>
         <input
           type="checkbox"
-          checked={showTilt}
-          onChange={e => setShowTilt(e.target.checked)}
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
         />
-        {' show tilt'}
+        {' show globe'}
       </label>
-      {showTilt ? <Tilt>This is tilted!</Tilt> : null}
+      <div style={{width: 400, height: 400}}>
+        {showGlobe ? <Globe /> : null}
+      </div>
     </div>
   )
 }
