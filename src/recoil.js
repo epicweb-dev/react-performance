631c8921de2bc40d3c30801f6e2e5c06f8af5ca7
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-31 20:49:02 -0600

    update all the things

--- src/recoil.js
+++
@@ -1,5 +0,0 @@
-// the recoil utilities that I'm using aren't officially exported yet
-// so I've got this here and thanks to the jsconfig.json I can make my import
-// look like it's coming from the real recoil which is handy.
-
-export * from 'jared-recoil'
