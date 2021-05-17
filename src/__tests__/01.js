d0ebad574e0528ae46483a2b73560eaa1319dcf8
Emil Esplund <emil.esplund@tele2.com>
2021-05-17 14:15:51 +0200

    updated false-positive test for exercise 1

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
-import {render, screen, act} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from '../final/01'
 // import App from '../exercise/01'
@@ -16,18 +16,15 @@ beforeEach(() => {
   }
 })
 
-test('loads the tile component asynchronously', async () => {
+test('loads the globe component asynchronously', async () => {
   render(<App />)
+
+  userEvent.click(screen.getByLabelText(/show globe/))
 
   alfredTip(
     () => expect(screen.queryByTitle(/globe/i)).not.toBeInTheDocument(),
-    'The tilt component must be loaded asynchronously via React.lazy and React.Suspense',
+    'The globe component must be loaded asynchronously via React.lazy and React.Suspense',
   )
-
-  // TODO: figure out why act is needed here because it should not be...
-  await act(async () => {
-    userEvent.click(screen.getByLabelText(/show globe/))
-  })
 
   expect(await screen.findByTitle(/globe/i)).toBeInTheDocument()
 })
