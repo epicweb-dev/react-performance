363bb682abf8fad6071df02c2b565d9df1fa1ecd
Damian Żygadło <vvwill4u@gmail.com>
2021-12-04 13:39:17 +0100

    Minor type change in useDebouncedState

--- src/utils.tsx
+++ src/utils.tsx
@@ -147,7 +147,9 @@ function debounce<T>(cb: React.Dispatch<T>, time: number) {
 
 // this only needs to exist because concurrent mode isn't here yet. When we get
 // that then so much of our hack-perf fixes go away!
-function useDebouncedState<T>(initialState: T): [T, (...args: T[]) => void] {
+function useDebouncedState<T>(
+  initialState: T,
+): [T, (...args: React.SetStateAction<T>[]) => void] {
   const [state, setState] = React.useState(initialState)
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSetState = React.useCallback(debounce(setState, 200), [])
