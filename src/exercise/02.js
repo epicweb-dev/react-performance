2b9cf45ce2eb74138167773b70e0d74c63cdcec8
jeruk <mikurej95@gmail.com>
2024-01-31 21:16:57 -0500

    memoize getItems call

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -61,7 +61,7 @@ function App() {
   const [inputValue, setInputValue] = React.useState('')
 
   // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
   const items = allItems.slice(0, 100)
 
   const {
