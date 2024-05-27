298e670b6934765c24e7e52106322df108963ed3
Kent C. Dodds <me+github@kentcdodds.com>
2024-05-27 11:21:53 -0600

    upgrade to @epic-web/config

---
+++ epicshop/post-set-playground.js
@@ -0,0 +1,28 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+const exclude = [
+	'exercises/04.',
+	'exercises/05.',
+	'exercises/06.',
+	'exercises/07.',
+]
+
+if (exclude.every(e => !process.env.EPICSHOP_PLAYGROUND_SRC_DIR.includes(e))) {
+	fs.writeFileSync(
+		path.join(process.env.EPICSHOP_PLAYGROUND_DEST_DIR, 'tsconfig.json'),
+		JSON.stringify(
+			{
+				include: ['**/*.ts', '**/*.tsx'],
+				extends: ['@epic-web/config/typescript'],
+				compilerOptions: {
+					paths: {
+						'#*': ['../shared/*'],
+					},
+				},
+			},
+			null,
+			2,
+		),
+	)
+}
--- exercises/05.calculations/03.problem.async/src/reset.d.ts
+++ exercises/05.calculations/03.problem.async/src/reset.d.ts
@@ -1,10 +1,1 @@
-import '@total-typescript/ts-reset'
-import '@total-typescript/ts-reset/dom'
-
-import 'react'
-
-declare module 'react' {
-	interface CSSProperties {
-		[key: `--${string}`]: string | number
-	}
-}
+import '@epic-web/config/reset.d.ts'
